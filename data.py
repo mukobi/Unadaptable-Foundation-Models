6f19e7344ea9f498cadd58126053f5517eb600ca
Joshuaclymer <joshmclymer@gmail.com>
2024-04-03 14:23:34 +0000

    added loading hf datasets to data.py

--- data.py
+++ data.py
@@ -69,3 +69,40 @@ def get_fashion_mnist_data(
     )
 
     return fashion_mnist_train_loader, fashion_mnist_test_loader
+
+
+def get_huggingface_data(
+    dataset_path: str,
+    dataset_subset: str,
+    batch_size: int = 128, 
+    # test_batch_size: int = 1000
+) -> torch.utils.data.DataLoader:
+    """
+    Loads a Hugging Face dataset given its path and subset.
+    NOTE: you may have to split the dataset after loading.
+
+    Args:
+        dataset_path (str): The path to the Hugging Face dataset.
+        dataset_subset (str): The subset of the dataset to load.
+        batch_size (int, optional): The batch size for training data. Defaults to 128.
+        test_batch_size (int, optional): The batch size for test data. Defaults to 1000.
+
+    Returns:
+        torch.utils.data.DataLoader: A PyTorch DataLoader object containing the loaded dataset.
+    """
+
+    dataset = huggingface_datasets.load_dataset(dataset_path, dataset_subset)
+
+    dataset_loader = torch.utils.data.DataLoader(
+        dataset,
+        batch_size=batch_size,
+        shuffle=True,
+    )
+
+    # test_dataset = torch.utils.data.DataLoader(
+    #     dataset['test'],
+    #     batch_size=test_batch_size,
+    #     shuffle=True,
+    # )
+
+    return dataset_loader
---
+++ test_data.py
@@ -0,0 +1,29 @@
+from typing import Tuple
+
+import datasets as huggingface_datasets
+import torch.utils.data
+import torch
+import unittest
+from data import get_huggingface_data
+
+class TestData(unittest.TestCase):
+    def test_get_huggingface_data(self):
+        dataset_path = 'cais/wmdp-corpora'
+        dataset_subset = 'cyber-forget-corpus'
+        batch_size = 128
+        # test_batch_size = 1000
+
+        loader = get_huggingface_data(dataset_path, dataset_subset, batch_size)
+
+        # Assert that the train_loader and test_loader are instances of torch.utils.data.DataLoader
+        self.assertIsInstance(loader, torch.utils.data.DataLoader)
+
+        # Assert that the train_loader and test_loader have the expected batch sizes
+        self.assertEqual(len(loader.batch_sampler), batch_size)
+        # self.assertEqual(len(test_loader.batch_sampler), test_batch_size)
+
+        # Add more assertions as needed to validate the behavior of the function
+
+if __name__ == '__main__':
+    unittest.main()
+
